a624c3cc86aa1ca3fa01e73b2c814db159af0fbf
unknown <cookiesarenightmares@gmail.com>
2022-07-14 21:45:44 +0200

    initial commit

---
+++ public/main.js
@@ -0,0 +1,17 @@
+const {app, BrowserWindow} = require('electron')
+
+function createWindow(){
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+
+        }
+        
+    })
+    win.loadURL('http://localhost:3000')
+
+}
+
+app.on('ready', createWindow);
+
